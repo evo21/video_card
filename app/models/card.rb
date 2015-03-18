5a51d1b4186a299d75238e0186b251112d9527d1
Evan Koch <evankoch@gmail.com>
2015-03-18 16:24:12 -0400

    basic invitations model. Added index page for Info, site/card examples

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,7 +1,12 @@
 class Card < ActiveRecord::Base
   has_many :videos
+  has_many :invitations
 
   validates :slug, presence:true, uniqueness: true
+
+
+
+  CARD_CATEGORIES = ['birthday', 'congratulations', 'wedding', 'sympathy', 'thanks']
 
   # def to_param
   #   self.slug.downcase.gsub(" ", "-")
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150315001025) do
+ActiveRecord::Schema.define(version: 20150318183113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "cards", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "title"
     t.string   "slug"
+    t.string   "category_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "invitations", force: :cascade do |t|
+    t.string   "email"
+    t.text     "plug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "videos", force: :cascade do |t|
