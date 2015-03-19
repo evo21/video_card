586dfb74e70046b22f41bbf03e8d9f58cafcfc67
Evan Koch <evankoch@gmail.com>
2015-03-18 22:28:22 -0400

    add thumbnail elements to strong params for video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,7 @@
 class VideosController < ApplicationController
   
   def create
+    binding.pry
   	@card = Card.find(video_params[:card_id])
   	@video = Video.new(video_params)
   	@video.save
@@ -8,6 +9,6 @@ class VideosController < ApplicationController
   end
 
   def video_params
-  	params.require(:video).permit(:caption, :contributor, :card_id, :video_uuid)
+  	params.require(:video).permit(:caption, :contributor, :card_id, :video_uuid, :qvga => [:thumb, :small_thumb])
   end
 end
