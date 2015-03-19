9277f0f4fa8936414b055df7da925dcf0feea958
Evan Koch <evankoch@gmail.com>
2015-03-18 22:47:06 -0400

    added storage of thumbnails upon video creation in controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,14 +1,19 @@
 class VideosController < ApplicationController
   
   def create
-    binding.pry
   	@card = Card.find(video_params[:card_id])
+
   	@video = Video.new(video_params)
+
+        # going straight to the params hash because camera tag nests it within weird subhashes
+        @video.thumb = params[:video][:qvga][:thumb]
+        @video.small_thumb = params[:video][:qvga][:small_thumb]
+
   	@video.save
   	redirect_to card_path(@card)
   end
 
   def video_params
-  	params.require(:video).permit(:caption, :contributor, :card_id, :video_uuid, :qvga => [:thumb, :small_thumb])
+  	params.require(:video).permit(:caption, :contributor, :card_id, :video_uuid)
   end
 end
