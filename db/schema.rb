57d085079bf1c6daa6ec25f0472cf2fd2fcafc2e
Evan Koch <evankoch@gmail.com>
2015-03-24 00:13:11 -0400

    add background imgs to assets

--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,19 @@ ActiveRecord::Schema.define(version: 20150323234023) do
   enable_extension "plpgsql"
 
   create_table "cards", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "title"
     t.string   "slug"
+    t.string   "category_id"
     t.string   "category"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.text     "desc"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "invitations", force: :cascade do |t|
