32d05fceb56643119e9a7d0d958cf1cd7dbf7821
Evan Koch <evankoch@gmail.com>
2015-03-26 21:39:15 +0100

    Card style update

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -11,7 +11,7 @@ $( document ).ready(function() {
 
   $(window).on("backstretch.before", function (e, instance, index) {
     // If we wanted to stop the slideshow after it reached the end
-    if (index === 0) $("#hero").addClass("anniversary");
+    if (index === 0) $("#hero").removeClass("sympathy").addClass("anniversary");
     if (index === 1) $("#hero").removeClass("anniversary").addClass("basic");
     if (index === 2) $("#hero").removeClass("basic").addClass("congratulations");
     if (index === 3) $("#hero").removeClass("congratulations").addClass("anniversary");
